23515cfa67266d0f84bf1f6111bc440cfbf3873f
Leonardo Pereira <ldnpmarketing@gmail.com>
2023-07-05 19:51:24 +0100

    feat: add exercise 2

---
+++ src/estruturaCondicional/exercicio2.java
@@ -0,0 +1,19 @@
+package estruturaCondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+        number = sc.nextInt();
+        if ( number % 2 == 0 ) {
+            System.out.println("PAR");
+        } else {
+            System.out.println("IMPAR");
+        }
+        sc.close();
+    }
+}
